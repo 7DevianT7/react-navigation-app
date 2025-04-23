a870061fdb0af51bfd5491a80396b56e0248b581
7DevianT7 <bojan8522@gmail.com>
2025-04-23 13:34:23 +0200

    Add cv.pdf

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css";
+import profileImg from "../assets/profile.png";
 
 const About = () => {
   return (
@@ -7,22 +8,37 @@ const About = () => {
       <h2 className="section-title">ABOUT ME</h2>
       <div className="about-content">
         <div className="about-left">
-          <img src="profile.png" alt="About me" />
+          <img src={profileImg} alt="About me" />
         </div>
         <div className="about-right">
           <h3>
             Hi, I am <span>Bojan Vukojevic</span>
           </h3>
           <ul>
-            <li><span>First Name:</span> Bojan</li>
-            <li><span>Last Name:</span> Vukojevic</li>
-            <li><span>Age:</span> 27 years</li>
-            <li><span>Nationality:</span> Serbos</li>
-            <li><span>Languages:</span> English, Serbian, Bosnian, Croatian, Šatrovački</li>
-            <li><span>Address:</span> 121 I.Andrića, Banja Luka, Bosnia</li>
-            <li><span>Freelance:</span> Available</li>
+            <li>
+              <span>First Name:</span> Bojan
+            </li>
+            <li>
+              <span>Last Name:</span> Vukojevic
+            </li>
+            <li>
+              <span>Age:</span> 27 years
+            </li>
+            <li>
+              <span>Nationality:</span> Serbos
+            </li>
+            <li>
+              <span>Languages:</span> English, Serbian, Bosnian, Croatian,
+              Šatrovački
+            </li>
+            <li>
+              <span>Address:</span> 121 I.Andrića, Banja Luka, Bosnia
+            </li>
+            <li>
+              <span>Freelance:</span> Available
+            </li>
           </ul>
-          <a href="/cv.pdf" download>
+          <a href="/react-navigation-app/cv.pdf" download>
             <button className="resume-btn">DOWNLOAD RESUME</button>
           </a>
         </div>
